272d9ad03312dbd2213bb1636663964961b8d4e7
tom99763 <69715105+tom99763@users.noreply.github.com>
2020-10-26 13:44:20 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,17 +7,19 @@ def run():
     s = env.reset()
     while env.done == False:
         if s[1] == 'O':
-            action = t.search(s, env)
+            action = t.search()
             s_, r, done, info = env.step(action)
         else:
             print(env.available_actions())
             action = int(input(''))
             s_, r, done, info = env.step(action)
+            t.to_child(s_)
         env.render()
-        s = s_
+    s = s_
+    t.reset_root()
 
 
 if __name__ == "__main__":
     env = TicTacToeEnv()
-    t = MonteCarloTreeSearch(150000, player='O')
+    t = MonteCarloTreeSearch(30000, player='O')
     run()
